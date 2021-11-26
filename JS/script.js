cca481cff86fb60c4ece2f32b95bb2abc794ab08
bikesh rai <bikeshrai78@gmail.com>
2021-11-26 21:14:40 +0545

    Create cardId item for each milbrary array

--- JS/script.js
+++ JS/script.js
@@ -9,11 +9,13 @@ function returnLastItem(array){ //returs last element of an array
     return array[array.length - 1];
 }
 
-let cardCounter = 1;
+let myLibrary = [];
+// let cardCounter = myLibrary.length;
 Book.prototype.display = function(){
-    cardId = 'card'+ cardCounter;
-    createCard(cardId,this.readStatus); //creates card with unique id to show book information
-    cardCounter++;
+    // cardId = 'card'+ cardCounter;
+    this.cardId = `card${myLibrary.length}`;
+    createCard(this.cardId,this.readStatus); //creates card with unique id to show book information
+    
     let bookTitle = document.createElement('p');
     bookTitle.textContent = this.title;
     bookTitle.setAttribute('class', 'bookTitle');
@@ -23,7 +25,7 @@ Book.prototype.display = function(){
     let bookNumberOfPages = document.createElement('p');
     bookNumberOfPages.textContent = `Page Count: ${this.numberOfPages}`;
     bookNumberOfPages.setAttribute('class','bookNumberOfPages');
-    appendToParent(`#${cardId}`, bookTitle,bookAuthor,bookNumberOfPages);
+    appendToParent(`#${this.cardId}`, bookTitle,bookAuthor,bookNumberOfPages);
 }
 
 function appendToParent(parentNode,node1, node2, node3){
@@ -32,14 +34,15 @@ function appendToParent(parentNode,node1, node2, node3){
     document.querySelector(`${parentNode}`).appendChild(node3);
 }
 
-let myLibrary = [];
+
 function addBookToLibrary(){
     let title = document.getElementsByName('title')[0].value;
     let author = document.getElementsByName('author')[0].value;
     let numberOfPages =document.getElementsByName('page-number')[0].value;
     let readStatus = getRadioInputValue(document.getElementsByName('read-status'));
     myLibrary.push(new Book(title,author,numberOfPages,readStatus));
-    // console.table(myLibrary);
+    returnLastItem(myLibrary).display();
+    console.table(myLibrary);
     
 }
 
@@ -63,7 +66,7 @@ document.querySelector('#myForm').addEventListener('submit',(e) => {
     addBookToLibrary();
     clearInputField();
     popUpForm.style.display= 'none';
-    returnLastItem(myLibrary).display();
+    // returnLastItem(myLibrary).display();
 });
 function clearInputField(){
     document.getElementsByName('title')[0].value = '';
@@ -87,6 +90,7 @@ function createCard(idName,readStatus){
 
     const readButton = card.childNodes; //Creates live nodelist that updates itself when new DOM is added
     changeReadStatus(readButton);
+    console.log(readButton);
     
     let deleteBtn = document.createElement('button');
     deleteBtn.setAttribute('class','deleteBtn');
@@ -95,6 +99,8 @@ function createCard(idName,readStatus){
     // Delete card
     deleteBtn.addEventListener('click',(e)=> {
         parentCard = deleteBtn.parentNode;
+        deleteBtn.getAttribute('id');
+        console.log(parentCard.getAttribute('id'));
         document.querySelector('.container').removeChild(parentCard);
     });
 }
